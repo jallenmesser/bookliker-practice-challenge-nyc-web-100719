707f03d0228d2dbbc1ee07089aa2cb072c712b5d
jallenmesser <jallenmessersmith@gmail.com>
2020-02-24 17:04:22 -0500

    working without bonus

--- js/index.js
+++ js/index.js
@@ -1,21 +1,24 @@
+// This is where the inital Fetch is run to get the books. 
+
 document.addEventListener("DOMContentLoaded", function() {
-let BOOKSURL = 'http://localhost:3000/books'
+    let BOOKSURL = 'http://localhost:3000/books'
 
-fetch(BOOKSURL)
-.then(resp => resp.json())
-.then(books => books.forEach(renderBooks))
-
-let listPanel = document.getElementById('list-panel')
-
-listPanel.addEventListener('click', function(e){
-    fetch(BOOKSURL+`/${e.target.id}`)
+    fetch(BOOKSURL)
     .then(resp => resp.json())
-    .then(book => renderSelectedBook(book))
+    .then(books => books.forEach(renderBooks))
+
+    let listPanel = document.getElementById('list-panel')
+
+    //this renders the correct book to the middle of the page
+    listPanel.addEventListener('click', function(e){
+        fetch(BOOKSURL+`/${e.target.id}`)
+        .then(resp => resp.json())
+        .then(book => renderSelectedBook(book))
+    })
 })
-})
 
 
-
+// this is the function to render the book list
 let renderBooks = book => {
     let listPanel = document.getElementById('list-panel')
     let li = document.createElement('li')
@@ -23,6 +26,8 @@ let renderBooks = book => {
     li.innerText = `${book.title}`
     listPanel.append(li)
 }
+
+// this is the function for the rendering of the selected book
 
 let renderSelectedBook = book => {
     let showPanel = document.getElementById('show-panel')
@@ -43,17 +48,19 @@ let renderSelectedBook = book => {
     })
     let readButton = showPanel.getElementsByClassName('button')[0]
 
+    // this is the patch request to add the reader to the database
+
+    let newUsers = [...book.users, {"id":1, "username":"pouros"} ]
+    
     readButton.addEventListener('click', function(e){
         fetch(`http://localhost:3000/books/${e.target.dataset.id}`, {headers: { "Content-Type": "application/json; charset=utf-8" },
         method: 'PATCH',
         body: JSON.stringify({
-            "users": [ 
-              {"id":1, "username":"pouros"}
-            ]
+            "users": newUsers
           })
       })
       .then(resp => resp.json())
-      .then(book => console.log(book))
+      .then(book => renderSelectedBook(book))
     
     })}
 
